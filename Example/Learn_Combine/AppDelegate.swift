432de3965c0efc495059388365b07719f1db55b7
Sunset Wan <sunset.wan@assaabloy.com>
2023-02-09 09:45:33 +0800

    fix: add code

--- Example/Learn_Combine/AppDelegate.swift
+++ Example/Learn_Combine/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow()
-//        window.rootViewController = UINavigationController(rootViewController: ViewController())
-        window.rootViewController = VCLifeCycleViewController()
+        window.rootViewController = UINavigationController(rootViewController: VCLifeCycleViewController())
+//        window.rootViewController = VCLifeCycleViewController()
         window.makeKeyAndVisible()
         self.window = window
         return true
--- Example/Learn_Combine/VCLifeCycleViewController.swift
+++ Example/Learn_Combine/VCLifeCycleViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class VCLifeCycleBaseViewController: UIViewController {
 
     var vcName: String {
-        "defaultName"
+        get {
+            "defaultName"
+        }
     }
 
     override func viewDidLoad() {
@@ -39,6 +41,16 @@ class VCLifeCycleBaseViewController: UIViewController {
         log()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        log()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        log()
+    }
+
     func log(_ function: String = #function) {
         print("👹 \(vcName): " + function)
     }
@@ -50,10 +62,101 @@ class VCLifeCycleViewController: VCLifeCycleBaseViewController {
         "A"
     }
 
-    let pushButton
+    let pushButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
+        configureEmptyHouse()
+    }
+
+    private func configureEmptyHouse() {
+        pushButton.setTitle("Push B", for: .normal)
+        pushButton.setTitleColor(.blue, for: .normal)
+        pushButton.translatesAutoresizingMaskIntoConstraints = false
+        pushButton.addTarget(self, action: #selector(pushButtonDidPress), for: .touchUpInside)
+
+        view.addSubview(pushButton)
+
+        NSLayoutConstraint.activate([
+            pushButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pushButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    @objc private func pushButtonDidPress() {
+        let bVC = VCLifeCycleViewControllerB()
+        let container = UINavigationController(rootViewController: bVC)
+//      navigationController?.pushViewController(bVC, animated: true)
+        container.modalPresentationStyle = .fullScreen
+        present(container, animated: true)
+
+    }
+}
+
+
+class VCLifeCycleViewControllerB: VCLifeCycleBaseViewController {
+    override var vcName: String {
+        "B"
+    }
+
+    let pushButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue.withAlphaComponent(0.3)
+        configureEmptyHouse()
+    }
+
+    private func configureEmptyHouse() {
+        pushButton.setTitle("Push C", for: .normal)
+        pushButton.setTitleColor(.black, for: .normal)
+        pushButton.translatesAutoresizingMaskIntoConstraints = false
+        pushButton.addTarget(self, action: #selector(pushButtonDidPress), for: .touchUpInside)
+
+        view.addSubview(pushButton)
+
+        NSLayoutConstraint.activate([
+            pushButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pushButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    @objc private func pushButtonDidPress() {
+//        dismiss(animated: true)
+        let cVC = VCLifeCycleViewControllerC()
+        navigationController?.pushViewController(cVC, animated: true)
+    }
+}
+
+class VCLifeCycleViewControllerC: VCLifeCycleBaseViewController {
+    override var vcName: String {
+        "C"
+    }
+
+    let pushButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue.withAlphaComponent(0.3)
+        configureEmptyHouse()
+    }
+
+    private func configureEmptyHouse() {
+        pushButton.setTitle("Dismiss C", for: .normal)
+        pushButton.setTitleColor(.black, for: .normal)
+        pushButton.translatesAutoresizingMaskIntoConstraints = false
+        pushButton.addTarget(self, action: #selector(pushButtonDidPress), for: .touchUpInside)
+
+        view.addSubview(pushButton)
+
+        NSLayoutConstraint.activate([
+            pushButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pushButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    @objc private func pushButtonDidPress() {
+        dismiss(animated: true)
     }
 }
